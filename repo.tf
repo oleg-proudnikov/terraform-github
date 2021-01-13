b8cd536170785a85829a300aa42a0d5259f6805c
oleg-proudnikov <oleg.dev.mx@gmail.com>
2021-01-13 17:28:51 +0000

    [OP-1] Add java-exp and java-networking

--- repo.tf
+++ repo.tf
@@ -12,3 +12,33 @@ module "terraform_github" {
   template_repo  = module.basic_repo.name
 }
 
+module "java_exp" {
+  source = "./modules/repo"
+
+  name        = "java-exp"
+  description = "Java experiments"
+  is_private  = false
+  topics = [
+    "managed",
+    "java",
+    "basic",
+  "maven"]
+  template_owner = var.owner
+  template_repo  = module.basic_java.name
+}
+
+module "java_networking" {
+  source = "./modules/repo"
+
+  name        = "java-networking"
+  description = "Java networking experiments"
+  is_private  = false
+  topics = [
+    "managed",
+    "java",
+    "basic",
+  "maven"]
+  template_owner = var.owner
+  template_repo  = "basic-java"
+}
+
